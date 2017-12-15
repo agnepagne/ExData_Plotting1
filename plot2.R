2b52c8158f47889204013322ecdedeeae75b4cef
Agne <agne.larsson@gmail.com>
2017-12-16 00:24:26 +0100

    Plot 2

--- plot2.R
+++ plot2.R
@@ -6,10 +6,9 @@ household <- read.csv(file = "../household_power_consumption.txt",
 household$datetime <- strptime(paste(household$Date, household$Time), "%d/%m/%Y %H:%M:%S")
 household <- subset(household, as.Date(datetime) %in% c(as.Date("2007-02-01"), as.Date("2007-02-02")))
 
-png("plot1.png", width = 480, height = 480)
+png("plot2.png", width = 480, height = 480)
 
-hist(household$Global_active_power, col = "red",
-     xlab = "Global Active Power (kilowatts)",
-     main = "Global Active Power")
+with(household, { plot(datetime, Global_active_power, type = "n", xlab = "", ylab = "Global Active Power (kilowatts)")
+                  lines(datetime, Global_active_power)  })
 
 dev.off()
